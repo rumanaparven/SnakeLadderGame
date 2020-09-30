12115674ca1f365034f03d4670e8592131cd6939
Rumana Parven <rumanaparven16@gmail.com>
2020-09-29 22:56:59 -0700

    [Rumana] Add. Repeat till player reaches winning 100

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -12,24 +12,31 @@ namespace SnakeLadder
             int playerPosition = 0;
             Console.WriteLine("Player is at position : " + playerPosition);
             Random ran = new Random();
-            int die = ran.Next(1,7);
-            int option = ran.Next(0, 3);
-            Console.WriteLine("Dice roll gives : " + die);
-            switch (option)
+            
+            while (playerPosition != 100)
             {
-                case SNAKE:
-                    Console.WriteLine("Player bitten by snake");
-                    playerPosition -= die;
-                    break;
-                case LADDER:
-                    Console.WriteLine("Player climbs the ladder");
-                    playerPosition += die;
-                    break;
-                default:
-                    Console.WriteLine("Player choses not to play");
-                    break;
+                int die = ran.Next(1, 7);
+                int option = ran.Next(0, 3);
+                Console.WriteLine("Dice roll gives : " + die);
+                switch (option)
+                {
+                    case SNAKE:
+                        Console.WriteLine("Player bitten by snake");
+                        playerPosition -= die;
+                        break;
+                    case LADDER:
+                        Console.WriteLine("Player climbs the ladder");
+                        playerPosition += die;
+                        break;
+                    default:
+                        Console.WriteLine("Player choses not to play");
+                        break;
+                        
+                }
+                if (playerPosition < 0)
+                    playerPosition = 0;
+                Console.WriteLine("Player is now at position : " + playerPosition);
             }
-            Console.WriteLine("Player is now at position : " + playerPosition);
         }
     }
 }
