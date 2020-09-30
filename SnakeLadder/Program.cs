84c4804be66dec7644ae0e85e04012757f30e98a
Rumana Parven <rumanaparven16@gmail.com>
2020-09-30 00:26:30 -0700

    [Rumana] Add. Play the game with two players.

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace SnakeLadder
 {
@@ -6,46 +7,62 @@ namespace SnakeLadder
     {
         const int SNAKE = 1;
         const int LADDER = 2;
-        static void Main(string[] args)
+        public static int TwoPlayers(int playerPosition)
         {
 
-            int playerPosition = 0;
-            Console.WriteLine("Player is at position : " + playerPosition);
             Random ran = new Random();
-            int noOfTimesDiceRolled = 0;
+     
             while (playerPosition != 100)
             {
-                
+                int ifSnake = 0;
                 int die = ran.Next(1, 7);
-                noOfTimesDiceRolled++;
-                Console.WriteLine("Number of times dice rolled : " + noOfTimesDiceRolled);
+             
                 int option = ran.Next(0, 3);
-                Console.WriteLine("Dice roll gives : " + die);
                 
                     switch (option)
                     {
                         case SNAKE:
-                            Console.WriteLine("Player bitten by snake");
+                            //Console.WriteLine("Player bitten by snake");
                             playerPosition -= die;
+                            if (playerPosition < 0)
+                                playerPosition = 0;
+                            return playerPosition;
                             break;
                         case LADDER:
-                            Console.WriteLine("Player climbs the ladder");
+                            //Console.WriteLine("Player climbs the ladder");
                             playerPosition += die;
-                            break;
+                            if (playerPosition > 100)
+                                playerPosition = playerPosition - die;
+                        break;
                         default:
-                            Console.WriteLine("Player choses not to play");
+                            //Console.WriteLine("Player choses not to play");
                             break;
 
                     }
-                    if (playerPosition < 0)
-                        playerPosition = 0;
-                if (playerPosition >100)
-                    playerPosition = playerPosition-die;
 
-
-                Console.WriteLine("Player is now at position : " + playerPosition);
-                Console.WriteLine(" ");
+                    
+                    
+              
             }
+            return 100;
+        }
+
+        static void Main(string[] args)
+        {
+            int player1 = 0, player2 = 0;
+            Console.WriteLine("Player 1 is at position : " + player1);
+            Console.WriteLine("Player 2 is at position : " + player2);
+            while (player1<100 && player2<100)
+            {
+                player1= TwoPlayers(player1);
+                Console.WriteLine("Player 1 is at position : " + player1);
+                player2= TwoPlayers(player2);
+                Console.WriteLine("Player 2 is at position : " + player2);
+            }
+            if (player1 == 100)
+                Console.WriteLine("Player 1 is the winner.");
+            else
+                Console.WriteLine("Player 2 is the winner.");
         }
     }
 }
