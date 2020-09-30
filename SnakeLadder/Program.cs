5d2d970dd7447cc3ededcbd7a9e8604d4f61e111
Rumana Parven <rumanaparven16@gmail.com>
2020-09-29 22:12:55 -0700

    [Rumana] Add. Player rolls die

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -7,7 +7,12 @@ namespace SnakeLadder
         static void Main(string[] args)
         {
             int playerPosition = 0;
-            Console.WriteLine("Snake and Ladder game played with single player at position "+playerPosition);
+            Console.WriteLine("Player is at position : " + playerPosition);
+            Random ran = new Random();
+            int die = ran.Next(1, 7);
+            Console.WriteLine("Dice roll gives : " + die);
+            playerPosition += die;
+            Console.WriteLine("Player is now at position : " + playerPosition);
         }
     }
 }
