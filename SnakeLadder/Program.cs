50cfe2413fd723d4135602c6b06f0e44925564df
Rumana Parven <rumanaparven16@gmail.com>
2020-09-29 22:25:37 -0700

    [Rumana] Add. Player checks for option

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -4,14 +4,31 @@ namespace SnakeLadder
 {
     class Program
     {
+        const int SNAKE = 1;
+        const int LADDER = 2;
         static void Main(string[] args)
         {
+
             int playerPosition = 0;
             Console.WriteLine("Player is at position : " + playerPosition);
             Random ran = new Random();
-            int die = ran.Next(1, 7);
+            int die = ran.Next(1,7);
+            int option = ran.Next(0, 3);
             Console.WriteLine("Dice roll gives : " + die);
-            playerPosition += die;
+            switch (option)
+            {
+                case SNAKE:
+                    Console.WriteLine("Player bitten by snake");
+                    playerPosition -= die;
+                    break;
+                case LADDER:
+                    Console.WriteLine("Player climbs the ladder");
+                    playerPosition += die;
+                    break;
+                default:
+                    Console.WriteLine("Player choses not to play");
+                    break;
+            }
             Console.WriteLine("Player is now at position : " + playerPosition);
         }
     }
